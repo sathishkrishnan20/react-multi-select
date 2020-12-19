a43b42fae2da64b33d1fe97c2dd3c19c719c1878
Sathish Krishnan <sathiskrish20@gmail.com>
2020-12-19 12:58:00 +0530

    updated-css-for-chips

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { MultiSelect } from './component/multi-select';
 import { Chips } from './component/chips';
--- src/component/chips.tsx
+++ src/component/chips.tsx
@@ -9,7 +9,7 @@ export function Chips({items, onClickRemove}: ChipsProp) {
                 <div className="chip-container" key={key}>
                     <div style={{ display: 'flex', flexDirection: 'row'}}>
                         <div className={'label'}> {item.label}</div>
-                        <span onClick={() => onClickRemove(item.value)} className="closebtn">&times;</span>
+                        <i onClick={() => onClickRemove(item.value)} className="material-icons closebtn">close</i>
                     </div>
                 </div>
             ))}
